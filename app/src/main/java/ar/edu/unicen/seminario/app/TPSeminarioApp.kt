c622827903b2f688ecb5fe0667ebaa5cc3944db1
Sofia Mondello <sofiarmondello@gmail.com>
2024-10-31 21:13:16 -0300

    modo claro, oscuro, idioma español, ingles y cambio logo app

--- app/src/main/java/ar/edu/unicen/seminario/app/TPSeminarioApp.kt
+++ app/src/main/java/ar/edu/unicen/seminario/app/TPSeminarioApp.kt
@@ -1,8 +1,9 @@
 package ar.edu.unicen.seminario.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 
-class TPSeminarioApp: Application()
+class PeliculasDB: Application()
